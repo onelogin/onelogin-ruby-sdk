4efcf068fcadfe90856c9ac3950b2a8c80dabee4
Przemysław Dąbek <przemyslaw.dabek@uxpin.com>
2017-11-18 18:18:21 +0100

    added extra two fields for Duo Security device type

--- lib/onelogin/api/models/device.rb
+++ lib/onelogin/api/models/device.rb
@@ -4,11 +4,13 @@ module OneLogin
 
       class Device
 
-        attr_accessor :id, :type
+        attr_accessor :id, :type, :duo_api_hostname, :duo_sig_request
 
         def initialize(data)
           @id = data['device_id']
           @type = data['device_type'].to_s
+          @duo_api_hostname = data['duo_api_hostname']
+          @duo_sig_request = data['duo_sig_request']
         end
       end
     end
