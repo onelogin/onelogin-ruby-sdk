c99fba3952a39c719fdf40616c0b5bce5064e39e
Patrick Cable <pat.cable@threatstack.com>
2018-02-24 12:05:25 -0500

    added title attribute

--- lib/onelogin/api/models/user_data.rb
+++ lib/onelogin/api/models/user_data.rb
@@ -6,7 +6,7 @@ module OneLogin
 
         attr_accessor :id, :external_id, :email, :username, :firstname, :lastname, :distinguished_name,
                       :phone, :company, :department, :status, :member_of, :samaccountname, :userprincipalname,
-                      :openid_name, :locale_code, :directory_id, :manager_ad_id, :trusted_idp_id
+                      :title, :openid_name, :locale_code, :directory_id, :manager_ad_id, :trusted_idp_id
       end
     end
   end
