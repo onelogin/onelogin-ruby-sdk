4356772c6b6dd978defb4c974bbd8d6564a91328
Rich Chetwynd <richardchetwynd@gmail.com>
2018-05-11 13:02:36 +1200

    simple update user example

---
+++ examples/rails-custom-login-page/app/controllers/users_controller.rb
@@ -0,0 +1,78 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = api_client.get_users.take(25) # only fetch the first 50 users
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    user = api_client.update_user(params[:id], user_params)
+    puts user
+
+    respond_to do |format|
+      if user
+        format.html { redirect_to user_path(params[:id]), notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = api_client.get_user(params[:id])
+      p @user
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.permit(:firstname, :lastname, :email, :phone)
+    end
+end
--- examples/rails-custom-login-page/config/routes.rb
+++ examples/rails-custom-login-page/config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
 
   get 'dashboard', to: 'dashboard#index'
 
+  get 'users', to: 'users#index'
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch 'users/:id', to: 'users#update', as: 'update_user'
+
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
